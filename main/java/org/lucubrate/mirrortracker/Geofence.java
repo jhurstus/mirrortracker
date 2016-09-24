0a3ed01c87b711386a90b373c988b9ebc54a9076
Joey Hurst <jhurstus@gmail.com>
2016-09-24 15:58:05 -0700

    Add Geofence POJO class.

---
+++ main/java/org/lucubrate/mirrortracker/Geofence.java
@@ -0,0 +1,17 @@
+package org.lucubrate.mirrortracker;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+/**
+ * POJO representing a location geofence.
+ */
+@IgnoreExtraProperties
+public class Geofence {
+    public double lat;
+    public double lng;
+    public float radius;
+
+    public Geofence() {}
+
+    public Geofence
+}

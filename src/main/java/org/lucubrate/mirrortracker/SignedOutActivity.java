f601b91d8d62f14dcfd334cc139663ad16c6c750
Joey Hurst <jhurstus@gmail.com>
2023-10-23 12:49:33 -0700

    Fix warnings in SignedOutActivity

--- src/main/java/org/lucubrate/mirrortracker/SignedOutActivity.java
+++ src/main/java/org/lucubrate/mirrortracker/SignedOutActivity.java
@@ -3,17 +3,24 @@ package org.lucubrate.mirrortracker;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 public class SignedOutActivity extends AppCompatActivity {
 
-    private final static int RC_SIGN_IN = 42;
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            this::onSignInResult
+    );
 
     static Intent createIntent(Context context) {
         Intent i = new Intent();
@@ -26,7 +33,7 @@ public class SignedOutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_signed_out);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.sign_in);
         setSupportActionBar(toolbar);
 
@@ -35,26 +42,21 @@ public class SignedOutActivity extends AppCompatActivity {
             startActivity(SignedInActivity.createIntent(this));
             finish();
         } else {
-            startActivityForResult(
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(Arrays.asList(
-                                    new AuthUI.IdpConfig.GoogleBuilder().build()))
-                            .setIsSmartLockEnabled(true)
-                            .build(),
-                    RC_SIGN_IN);
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(Collections.singletonList(
+                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                    .setIsSmartLockEnabled(true)
+                    .build();
+            signInLauncher.launch(signInIntent);
         }
         setContentView(R.layout.activity_signed_out);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            if (resultCode == RESULT_OK) {
-                startActivity(SignedInActivity.createIntent(this));
-                finish();
-            }
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        if (result.getResultCode() == RESULT_OK) {
+            startActivity(SignedInActivity.createIntent(this));
+            finish();
         }
     }
 }
